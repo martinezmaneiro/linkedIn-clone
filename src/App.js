8d306646594cc66f28ca47fb0d599c1175bd7a41
Joaquin Martinez <joaquinmar08@gmail.com>
2023-03-07 20:55:04 -0300

    [FIXED] redux store import

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './App.css';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice'
 import Header from './components/header/Header';
+import Login from './components/login/Login';
 import Sidebar from './components/sidebar/Sidebar';
 import Feed from './components/feed/Feed';
 
+
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header/>
+      {!user ?
+      (<Login/>)
+      :
+      (
       <div className='app-body'>
         <Sidebar/>
         <Feed/>
       </div>
+      )}
     </div>
   );
 }
